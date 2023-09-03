fb5b49189f6b5bce85757916cc37ca5d92bc8da9
K-Othman <karim_othman88@hotmail.com>
2023-09-03 19:14:19 +0100

    created the corners charts

--- src/components/corners/Corners.jsx
+++ src/components/corners/Corners.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { MatchDetailsContext } from "../../context/matchDetailsContext/MatchDetailsContext";
 import { Progress } from "@material-tailwind/react";
 
-const ShotsOnTarget = () => {
+const Corners = () => {
   const { matchesDetails } = useContext(MatchDetailsContext);
 
   if (
@@ -14,35 +14,37 @@ const ShotsOnTarget = () => {
   }
 
   const homeStat = matchesDetails.match.liveData.lineups.home.stats.find(
-    (stat) => stat.type === "ontargetScoringAtt" && stat.value
+    (stat) => stat.type === "wonCorners" && stat.value
   );
   const awayStat = matchesDetails.match.liveData.lineups.away.stats.find(
-    (stat) => stat.type === "ontargetScoringAtt" && stat.value
+    (stat) => stat.type === "wonCorners" && stat.value
   );
 
   if (!homeStat || !awayStat) {
     return <p>Possession data not available for this match.</p>;
   }
 
-  const homeOnTargetShots = Number(homeStat.value);
-  const awayOnTargetShots = Number(awayStat.value);
+  console.log(matchesDetails.match.liveData.lineups.home.stats);
 
-  const totalShots = homeOnTargetShots + homeOnTargetShots;
-  const homeOnTargetShotsPercentage = (homeOnTargetShots / totalShots) * 100;
-  const awayShotsOnTargetPercentage = (awayOnTargetShots / totalShots) * 100;
+  const homeCorener = Number(homeStat.value);
+  const awaycorner = Number(awayStat.value);
+
+  const totalCorners = homeCorener + awaycorner;
+  const HomeCornersPercentage = (homeCorener / totalCorners) * 100;
+  const awayCornersPercentage = (awaycorner / totalCorners) * 100;
 
   return (
     <div>
       <div>
-        <h3 className="text-center mt-4">Shots on Target</h3>
+        <h3 className="text-center mt-4">Corners</h3>
         <div className="flex gap-4">
           <div className="flex-1">
-            <span className="flex mb-2">{homeOnTargetShots}</span>
-            <Progress value={homeOnTargetShotsPercentage} color="amber" />
+            <span className="flex mb-2">{homeCorener}</span>
+            <Progress value={HomeCornersPercentage} color="amber" />
           </div>
           <div className="flex-1">
-            <span className="flex justify-end mb-2">{awayOnTargetShots}</span>
-            <Progress value={awayShotsOnTargetPercentage} color="green" />
+            <span className="flex justify-end mb-2">{awaycorner}</span>
+            <Progress value={awayCornersPercentage} color="green" />
           </div>
         </div>
       </div>
@@ -50,4 +52,4 @@ const ShotsOnTarget = () => {
   );
 };
 
-export default ShotsOnTarget;
+export default Corners;
--- src/components/fullTime/FullTime.jsx
+++ src/components/fullTime/FullTime.jsx
@@ -1,13 +1,15 @@
+import Corners from "../corners/Corners";
 import Possession from "../possession/Possession";
 import Shots from "../shots/Shots";
 import ShotsOnTarget from "../shotsOnTarget/ShotsOnTarget";
 
 const FullTime = () => {
   return (
-    <section className="px-2">
+    <section className="p-2 pb-5">
       <Possession />
       <Shots />
       <ShotsOnTarget />
+      <Corners />
     </section>
   );
 };
